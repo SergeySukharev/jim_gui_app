727a83bc47a585081f5a45e9caead3b02e6942c1
Сухарев Сергей Юрьевич <sysukhar@stream.local>
2021-01-11 15:54:24 +0300

    added 5 states

--- index.py
+++ index.py
@@ -1,6 +1,17 @@
+states = {
+    "ny": 0.3,
+    "la": 0.5,
+    "man": 0.2,
+    "org": 1.4,
+    "pen": 1.1
+}
 
+
+state = input('Введите код штата(например ny): ')
 sku = input('Введите количество единиц товара: ')
 price = input('Ввидите цену товара: ')
+sum = str(int(sku)*int(price))
+sum_tax = str(int(sum) + (int(sku)*int(price)*states[state]))
 
-print('Стоимость вашего заказа - ' + str(int(sku)*int(price)))
-
+print('Стоимость вашего заказа без налогов - ' + sum)
+print('Стоимость вашего заказа c учетом налога штата - ' + sum_tax)
