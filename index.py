5c92f8e4810f2085e353a94e467cad140d81a59d
Сухарев Сергей Юрьевич <sysukhar@stream.local>
2021-01-11 16:32:21 +0300

    added discount system

--- index.py
+++ index.py
@@ -8,10 +8,25 @@ states = {
 
 
 state = input('Введите код штата(например ny): ')
-sku = input('Введите количество единиц товара: ')
-price = input('Ввидите цену товара: ')
-sum = str(int(sku)*int(price))
-sum_tax = str(int(sum) + (int(sku)*int(price)*states[state]))
+sku = int(input('Введите количество единиц товара: '))
+price = int(input('Ввидите цену товара: '))
+summ = sku*price
+sum_tax = summ + sku*price*states[state]
+your_discount = 0
 
-print('Стоимость вашего заказа без налогов - ' + sum)
-print('Стоимость вашего заказа c учетом налога штата - ' + sum_tax)
+
+if summ >= 1000:
+    your_discount = summ * 0.03
+elif summ >= 5000:
+    your_discount = summ * 0.05
+elif summ >= 7000:
+    your_discount = summ * 0.07
+elif summ >= 10000:
+    your_discount = summ * 0.1
+elif summ >= 15000:
+    your_discount = summ * 0.15
+final_summ = sum_tax - your_discount
+
+print('Ваша скидка - ' + str(your_discount))
+print('Стоимость вашего заказа без налогов - ' + str(summ))
+print('Стоимость вашего заказа c учетом налога штата и учетом вашей скидки - ' + str(final_summ))
